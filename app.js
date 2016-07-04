f4d1f322d0a99470e70cbcba925042fc3b92991e
Jon Wilson <jonwilsoncomedy@gmail.com>
2016-07-04 18:52:29 -0500

    basic working version

--- app.js
+++ app.js
@@ -11,4 +11,19 @@ randomGIF.then(function(data) {
   $('#gifContainer').append($randomURLLink);
 });
 
+$('#getTagGIF').on('submit', function(event) {
+  event.preventDefault();
+  $('#gifContainer').empty();
+  var tagToAdd = $('#tagInput').val();
+  var tagAdder = 'http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC' + '&tag=' + tagToAdd;
+  randomGIF = $.get(tagAdder);
+
+  randomGIF.then(function(data) {
+    var randomURL = data.data.image_url;
+    var $randomURLLink = $('<img src="' + randomURL + '"</img>');
+    $('#gifContainer').append($randomURLLink);
+    console.log(data);
+  });
+});
+
 });
