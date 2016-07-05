f343752c71464a49b49b1413f32843d6a6cb79df
Jon Wilson <jonwilsoncomedy@gmail.com>
2016-07-04 22:57:40 -0500

    modified js to convert spaces in tags to plus signs

--- app.js
+++ app.js
@@ -15,14 +15,25 @@ $('#getTagGIF').on('submit', function(event) {
   event.preventDefault();
   $('#gifContainer').empty();
   var tagToAdd = $('#tagInput').val();
-  var tagAdder = 'http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC' + '&tag=' + tagToAdd;
+  var tagNoSpaces = tagToAdd.replace(/[ ]/g, "+");
+  console.log(tagNoSpaces);
+
+// for (var i = 0; i < tagToAdd.length; i++) {
+//   if(tagToAdd[i] === ' ') {
+//     tagToAdd[i] = '+';
+//     console.log(tagToAdd);
+//   };
+// };
+
+
+
+  var tagAdder = 'http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC' + '&tag=' + tagNoSpaces;
   randomGIF = $.get(tagAdder);
 
   randomGIF.then(function(data) {
     var randomURL = data.data.image_url;
     var $randomURLLink = $('<img src="' + randomURL + '"</img>');
     $('#gifContainer').append($randomURLLink);
-    console.log(data);
   });
 });
 
